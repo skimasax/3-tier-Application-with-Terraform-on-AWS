499ef5c9c2fa626e41e00198a8f5c0306d334478
Adeitan Abimbola Moses <93311300+skimasax@users.noreply.github.com>
2023-08-26 21:57:46 +0100

    updated

--- main.tf
+++ main.tf
@@ -7,3 +7,25 @@ resource "aws_vpc" "bimbo_vpc"{
   enable_dns_support = true
   enable_dns_hostnames = true
 }
+
+#create public subnet 1
+resource "aws_subnet" "public_subnet1"{
+  vpc_id                  = aws_vpc.checkpoint_vpc.id
+  cidr_block              = "10.0.0.0/24"  # Adjust the CIDR block as needed
+  availability_zone      = "us-east-1a"   # Change this to the desired AZ in your region
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Public Subnet1"
+  }
+}
+
+#create public subnet 2
+resource "aws_subnet" "public_subnet2"{
+  vpc_id                  = aws_vpc.checkpoint_vpc.id
+  cidr_block              = "10.0.1.0/24"  # Adjust the CIDR block as needed
+  availability_zone      = "us-east-1b"   # Change this to the desired AZ in your region
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Public Subnet2"
+  }
+}
---
+++ output.tf
@@ -0,0 +1,11 @@
+output "bimbo_vpc"{
+    value=aws_vpc.bimbo_vpc
+}
+
+output "bimbo_subnet1"{
+    value=aws_subnet.public_subnet1
+}
+
+output "bimbo_subnet2"{
+    value=aws_subnet.public_subnet2
+}
