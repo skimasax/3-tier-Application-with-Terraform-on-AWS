8756beec7b5c291c3c57c6fd90b767d56eed3a1a
Adeitan Abimbola Moses <93311300+skimasax@users.noreply.github.com>
2023-08-26 22:07:09 +0100

    updated

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ resource "aws_vpc" "bimbo_vpc"{
 #create public subnet 1
 resource "aws_subnet" "public_subnet1"{
   vpc_id                  = aws_vpc.bimbo_vpc.id
-  cidr_block              = "10.0.0.0/24"  # Adjust the CIDR block as needed
-  availability_zone      = "us-east-1a"   # Change this to the desired AZ in your region
+  cidr_block              = "10.0.0.0/24"  
+  availability_zone      = "us-east-1a"  
   map_public_ip_on_launch = true
   tags = {
     Name = "Public Subnet1"
@@ -22,8 +22,8 @@ resource "aws_subnet" "public_subnet1"{
 #create public subnet 2
 resource "aws_subnet" "public_subnet2"{
   vpc_id                  = aws_vpc.bimbo_vpc.id
-  cidr_block              = "10.0.1.0/24"  # Adjust the CIDR block as needed
-  availability_zone      = "us-east-1b"   # Change this to the desired AZ in your region
+  cidr_block              = "10.0.1.0/24"  
+  availability_zone      = "us-east-1b"   
   map_public_ip_on_launch = true
   tags = {
     Name = "Public Subnet2"
@@ -33,8 +33,8 @@ resource "aws_subnet" "public_subnet2"{
 #create Private subnets
 resource "aws_subnet" "private_subnet1"{
   vpc_id                  = aws_vpc.bimbo_vpc.id
-  cidr_block              = "10.0.2.0/24"  # Adjust the CIDR block as needed
-  availability_zone      = "us-east-1a"   # Change this to the desired AZ in your region
+  cidr_block              = "10.0.2.0/24"  
+  availability_zone      = "us-east-1a"  
   map_public_ip_on_launch = true
   tags = {
     Name = "Private Subnet1"
@@ -42,8 +42,8 @@ resource "aws_subnet" "private_subnet1"{
 }
 resource "aws_subnet" "private_subnet2"{
   vpc_id                  = aws_vpc.bimbo_vpc.id
-  cidr_block              = "10.0.3.0/24"  # Adjust the CIDR block as needed
-  availability_zone      = "us-east-1b"   # Change this to the desired AZ in your region
+  cidr_block              = "10.0.3.0/24"  
+  availability_zone      = "us-east-1b" 
   map_public_ip_on_launch = true
   tags = {
     Name = "Private Subnet3"
@@ -51,8 +51,8 @@ resource "aws_subnet" "private_subnet2"{
 }
 resource "aws_subnet" "private_subnet3"{
   vpc_id                  = aws_vpc.bimbo_vpc.id
-  cidr_block              = "10.0.4.0/24"  # Adjust the CIDR block as needed
-  availability_zone      = "us-east-1a"   # Change this to the desired AZ in your region
+  cidr_block              = "10.0.4.0/24"  
+  availability_zone      = "us-east-1a"   
   map_public_ip_on_launch = true
   tags = {
     Name = "Private Subnet3"
@@ -60,8 +60,8 @@ resource "aws_subnet" "private_subnet3"{
 }
 resource "aws_subnet" "private_subnet4"{
   vpc_id                  = aws_vpc.bimbo_vpc.id
-  cidr_block              = "10.0.5.0/24"  # Adjust the CIDR block as needed
-  availability_zone      = "us-east-1b"   # Change this to the desired AZ in your region
+  cidr_block              = "10.0.5.0/24"  
+  availability_zone      = "us-east-1b"   
   map_public_ip_on_launch = true
   tags = {
     Name = "Private Subnet4"
