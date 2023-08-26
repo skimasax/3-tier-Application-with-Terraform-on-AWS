a28b365073bbb1aa8f2544ae40f24576968c5404
Adeitan Abimbola Moses <93311300+skimasax@users.noreply.github.com>
2023-08-26 22:05:54 +0100

    updated

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "bimbo_vpc"{
 
 #create public subnet 1
 resource "aws_subnet" "public_subnet1"{
-  vpc_id                  = aws_vpc.checkpoint_vpc.id
+  vpc_id                  = aws_vpc.bimbo_vpc.id
   cidr_block              = "10.0.0.0/24"  # Adjust the CIDR block as needed
   availability_zone      = "us-east-1a"   # Change this to the desired AZ in your region
   map_public_ip_on_launch = true
@@ -21,11 +21,84 @@ resource "aws_subnet" "public_subnet1"{
 
 #create public subnet 2
 resource "aws_subnet" "public_subnet2"{
-  vpc_id                  = aws_vpc.checkpoint_vpc.id
+  vpc_id                  = aws_vpc.bimbo_vpc.id
   cidr_block              = "10.0.1.0/24"  # Adjust the CIDR block as needed
   availability_zone      = "us-east-1b"   # Change this to the desired AZ in your region
   map_public_ip_on_launch = true
   tags = {
     Name = "Public Subnet2"
   }
+}
+
+#create Private subnets
+resource "aws_subnet" "private_subnet1"{
+  vpc_id                  = aws_vpc.bimbo_vpc.id
+  cidr_block              = "10.0.2.0/24"  # Adjust the CIDR block as needed
+  availability_zone      = "us-east-1a"   # Change this to the desired AZ in your region
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Private Subnet1"
+  }
+}
+resource "aws_subnet" "private_subnet2"{
+  vpc_id                  = aws_vpc.bimbo_vpc.id
+  cidr_block              = "10.0.3.0/24"  # Adjust the CIDR block as needed
+  availability_zone      = "us-east-1b"   # Change this to the desired AZ in your region
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Private Subnet3"
+  }
+}
+resource "aws_subnet" "private_subnet3"{
+  vpc_id                  = aws_vpc.bimbo_vpc.id
+  cidr_block              = "10.0.4.0/24"  # Adjust the CIDR block as needed
+  availability_zone      = "us-east-1a"   # Change this to the desired AZ in your region
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Private Subnet3"
+  }
+}
+resource "aws_subnet" "private_subnet4"{
+  vpc_id                  = aws_vpc.bimbo_vpc.id
+  cidr_block              = "10.0.5.0/24"  # Adjust the CIDR block as needed
+  availability_zone      = "us-east-1b"   # Change this to the desired AZ in your region
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Private Subnet4"
+  }
+}
+
+
+# Create a security group
+resource "aws_security_group" "bimboSG" {
+  vpc_id     = aws_vpc.bimbo_vpc.id
+  name_prefix = "bimboPractise-sg"
+  description = "Checkpoint security group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#createKeypair
+resource "aws_key_pair" "my_key_pair" {
+  key_name   = "bimboPractise-keypair" 
+  public_key = file("~/.ssh/id_rsa.pub") 
 }
